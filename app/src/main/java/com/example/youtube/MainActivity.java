ce6a077f98e8da7aceacd6fc3e66fed74227e530
maayan3330 <maayan3330@gmail.com>
2024-06-02 18:25:55 +0300

    commit

--- app/src/main/java/com/example/youtube/MainActivity.java
+++ app/src/main/java/com/example/youtube/MainActivity.java
@@ -1,24 +1,25 @@
 package com.example.youtube;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // מציאת ה-ImageView עם ה-ID של הלוגו
+        ImageView logoImage = findViewById(R.id.logoImage);
+
+        // טעינת האנימציה מקובץ האנימציה
+        Animation flipAnimation = AnimationUtils.loadAnimation(this, R.anim.flip);
+
+        // התחלת האנימציה על הלוגו
+        logoImage.startAnimation(flipAnimation);
     }
 }
