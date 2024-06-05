e0fafa979bdfbd55541d471374a5eb460b2ef9cf
maayan3330 <maayan3330@gmail.com>
2024-06-05 16:34:05 +0300

    commit new 5.6

---
+++ app/src/main/java/com/example/youtube/User.java
@@ -0,0 +1,25 @@
+package com.example.youtube;
+
+public class User {
+    private String username;
+    private String password;
+    private String nickname;
+
+    public User(String username, String password, String nickname) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
